403e2d0b50893ffb1bd5820dbb40f66975093baf
Xander Stoffels <xander.s@outlook.com>
2024-05-11 17:47:21 +0900

    Init

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { createApp } from "vue";
+import { createMemoryHistory, createRouter } from "vue-router";
+
+import "./style.css";
+import App from "./App.vue";
+
+import Home from "./pages/Home.vue";
+import About from "./pages/About.vue";
+
+const routes = [
+  { path: "/", component: Home },
+  { path: "/about", component: About },
+];
+
+const router = createRouter({
+  history: createMemoryHistory(),
+  routes,
+});
+
+createApp(App).use(router).mount("#app");
